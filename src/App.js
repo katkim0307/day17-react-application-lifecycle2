3f622dbd0ddd906a9924abc857aaf09d55e11dce
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-30 13:47:45 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Lifecycle2 extends Component {
+  constructor() {
+    super();
+    this.state={
+      count: 0,
+      intervalID: 0,
+      pause: false,
+    };
+  };
 
-export default App;
+  componentDidMount () {
+    // start our ticker
+    let intervalID = setInterval(() => this.setState({count: this.state.count+1}), 1000);
+    this.setState ({ intervalID: intervalID })
+  };
+
+  // shouldComponentUpdate (nextProps, nextState) {
+  //   // We only want to show #s that are multiples of 3
+  //   // return this.state.count%3===0;
+  //   return nextState.count%3 === 0;
+  // };
+
+  pauseOnOff = () => {
+    this.setState ({pause: !this.state.pause});
+    if(!this.state.pause) {
+      clearInterval(this.state.intervalID);
+    } 
+    else {
+      this.componentDidMount(); 
+    }
+  };
+
+  clear = () => {
+    this.setState ({ count: 0 })
+  };
+
+  render() {
+    let {count, pause} = this.state;
+    return (
+      <div>
+        <p>The ticker is: {count}</p>
+        <button type="button" onClick={this.pauseOnOff}>{pause ? 'Resume':'Pause'}</button>
+        <button type="button" onClick={this.clear}>Reset</button>
+      </div>
+    )
+  };
+};
+
+export default Lifecycle2;
